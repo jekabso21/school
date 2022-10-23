ee3b73d200b02e321d4534b7facb00cebcc49787
retro <jekabsoss21@gmail.com>
2022-10-23 11:15:44 -0700

    added todo

--- bit_compressor.py
+++ bit_compressor.py
@@ -9,6 +9,7 @@ def sepprate_each_num(bit):
     res = compress()
     return res
 
+#TODO fix the range bug that when you reach the last digit it detects that there are no more on the right
 def compress():
     for i in range(len(bits)):
         if i <= len(bits):
@@ -22,18 +23,3 @@ def compress():
 
 print(sepprate_each_num(user_input))
 
-# str1 = "Geeks123for127geeks"
-#
-# # printing initial ini_string
-# print("initial string : ", str1)
-#
-# # using loop and in
-# # to remove numeric digits from string
-# num = "1234567890"
-# str2 = ""
-# for i in str1:
-#     if i not in num:
-#         str2 += i
-#
-# # printing result
-# print("final string : ", str2)
