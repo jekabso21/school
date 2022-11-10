adfdb6f444525b637c1ee206165e4c765295f77e
Retro21 <56453764+jekabso21@users.noreply.github.com>
2022-11-10 22:50:16 +0200

    Added commentes to each line

--- unique_num.py
+++ unique_num.py
@@ -1,15 +1,15 @@
-import re
+import re #import regex
 
-user_input = input("Enter your numbers: \n")
-unique = []
-
-
-def unique_num(nums):
-    numbs = re.findall('\-?\d+', nums)
-    for i in numbs:
-        if i not in unique:
-            unique.append(i)
-    return unique
+user_input = input("Enter your numbers: \n") #Gets user input
+unique = [] #saves the unique numbers
 
 
-print(unique_num(user_input))
+def unique_num(nums): #Starts the function
+    numbs = re.findall('\-?\d+', nums) #Finds all the numbers in the string
+    for i in numbs: #For each number in the string
+        if i not in unique: #If the number is not in the unique list
+            unique.append(i) #Add the number to the unique list
+    return unique #Return the unique list
+
+
+print(unique_num(user_input)) #Prints the returned list
